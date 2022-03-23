fc3fc7cc4d84e7ce39946ac387cfae1acad1d5ac
Boris Zarate <boris.zlobos@gmail.com>
2022-03-23 20:13:20 -0300

    formik con hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useFormik } from "formik"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const validate = (values) => {
+  const errors = {}
+
+  if (!values.name) {
+    errors.name = "requerido"
+  } else if (values.name.length < 5) {
+    errors.name = "requiere mas caracteres"
+  }
+  if (!values.lastname) {
+    errors.lastname = "requerido"
+  } else if (values.lastname.length < 5) {
+    errors.lastname = "requiere mas caracteres"
+  }
+  return errors
 }
 
-export default App;
+const App = () => {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      lastname: "",
+      email: "",
+    },
+    validate,
+    onSubmit: (values) => console.log(values),
+  })
+
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <label> Nombre </label>
+      <input {...formik.getFieldProps("name")} type="text" />
+      <br />
+      {formik.touched.name && formik.errors.name ? (
+        <div>{formik.errors.name}</div>
+      ) : null}
+      <label> Apellido</label>
+      <input {...formik.getFieldProps("lastname")} type="text" />
+      {formik.touched.lastname && formik.errors.lastname ? (
+        <div>{formik.errors.lastname}</div>
+      ) : null}
+      <br />
+      <label>Email</label>
+      <input {...formik.getFieldProps("email")} type="email" />
+      {formik.touched.email && formik.errors.email ? (
+        <div>{formik.errors.email}</div>
+      ) : null}
+      <button type="submit"> enviar</button>
+    </form>
+  )
+}
+
+export default App
